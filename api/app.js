89771c463e998aa8e44c9ccb1351f6965b1a4771
Oluwatoyosi Oyegoke <oluwatoyosi.oyegoke@gmail.com.com>
2021-06-21 15:32:38 +0100

    init commit with api folder

---
+++ api/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+
+
+require("dotenv").config()
+const port = process.env.PORT || 4000;
+const passport = require('passport')
+
+
+
+app.use(express.json({ limit: "50mb", }));
+app.use(express.urlencoded({ extended: true }))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(function (req, res, next) {
+    var allowedOrigins = [process.env.FRONTEND_URI];
+    var origin = req.headers.origin;
+
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader("Access-Control-Allow-Origin", origin); // restrict it to the required domain
+    }
+
+    res.header('Access-Control-Allow-Methods', 'GET, OPTIONS, PUT, PATCH, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', true);
+    return next();
+});
+
+
+app.listen(port, () => console.log(`listening on http://localhost:${port}`));
